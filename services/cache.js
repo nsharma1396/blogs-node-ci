60030485b5ed44a48f3ea71b4d17607be192a8ad
Neeraj <n.sharma1396@gmail.com>
2020-04-15 06:43:53 +0530

    Implement cache cleaning and expiration and complete cache functionality

--- services/cache.js
+++ services/cache.js
@@ -6,12 +6,14 @@ const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl, {
   password: process.env.REDIS_PASSWORD,
 });
-client.get = util.promisify(client.get).bind(client);
+client.hget = util.promisify(client.hget).bind(client);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || ""); // to cover all posibilities except of just user id as a top level key for the nested redis entry
+
   return this;
 };
 
@@ -29,11 +31,13 @@ mongoose.Query.prototype.exec = async function () {
   // console.log("Key", JSON.stringify(cacheKey));
   // try {
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(cacheKey);
+  const cacheValue = await client.hget(this.hashKey, cacheKey);
 
   // If we do, return that
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
+
+    console.log("Blogs found in cache");
 
     if (Array.isArray(doc)) {
       return doc.map((docItem) => new this.model(docItem));
@@ -45,12 +49,17 @@ mongoose.Query.prototype.exec = async function () {
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
 
-  console.log(cacheKey, JSON.stringify(result));
-
-  client.set(cacheKey, JSON.stringify(result));
+  client.hset(this.hashKey, cacheKey, JSON.stringify(result));
+  client.expire(this.hashKey, 10);
 
   return result;
   // } catch (err) {
   //   return exec.apply(this, arguments);
   // }
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
 };
