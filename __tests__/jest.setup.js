1fc2b8787618f5b82f4590e5cd22b4e255096b28
Neeraj <n.sharma1396@gmail.com>
2020-04-19 06:01:20 +0530

    Fix mongoose URI in jest setup

--- __tests__/jest.setup.js
+++ __tests__/jest.setup.js
@@ -5,8 +5,9 @@ require("dotenv").config();
 require("../models/User");
 
 const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(keys.mongoURI, {
   useMongoClient: true,
 });
